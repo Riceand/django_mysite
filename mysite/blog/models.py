af89a4a473a37f0c70511f4e8ad9b4b4f41535c5
Riceand <lixiangning016@163.com>
2018-07-26 15:58:55 +0800

    second

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,12 +4,18 @@ from django.contrib.auth.models import User
 
 class BlogType(models.Model):
     type_name = models.CharField(max_length=15)
-
+    
+    def __str__(self):
+        return self.type_name
+    
 
 class Blog(models.Model):
     title = models.CharField(max_length=50)
     blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)
     content = models.TextField()
-    auth = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
+    last_updated_time = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return "<Blog: %s>" % self.title
