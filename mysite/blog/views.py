e3a608e2f51bddf3d440556372757392e14fa7c3
Riceand <lixiangning016@163.com>
2018-08-01 11:31:45 +0800

    fourth-02

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,13 +8,13 @@ def blog_list(request):
     context['blogs'] = Blog.objects.all()
     # blogs count
     context['blog_count'] = Blog.objects.all().count()
-    return render_to_response('blog_list.html', context)
+    return render_to_response('blog/blog_list.html', context)
 
 
 def blog_detail(request, blog_pk):
     context = {}
     context['blog'] = get_object_or_404(Blog, pk=blog_pk)
-    return render_to_response('blog_detail.html', context)
+    return render_to_response('blog/blog_detail.html', context)
 
 
 def blogs_with_type(request, blog_tpye_pk):
@@ -22,4 +22,4 @@ def blogs_with_type(request, blog_tpye_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_tpye_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
-    return render_to_response('blogs_with_type.html', context)
+    return render_to_response('blog/blogs_with_type.html', context)
